e5707b97c4fd07fd07cf26d5ea0038012eb65c38
Vahnessa Gonzales <vahnessa.gonzales@gmail.com>
2025-05-23 03:06:24 +0800

    added content column to Book table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_21_192448) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_22_190339) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_21_192448) do
     t.string "download_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "content"
   end
 
   create_table "reading_progresses", force: :cascade do |t|
