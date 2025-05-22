ef14c73542f0f002567195427f48bb80eb051e12
Vahnessa Gonzales <vahnessa.gonzales@gmail.com>
2025-05-23 02:59:17 +0800

    setup dictionary api, modified books#index and books#show view

---
+++ app/wrappers/webster_api.rb
@@ -0,0 +1,26 @@
+class WebsterApi
+  include HTTParty
+  base_uri "https://www.dictionaryapi.com/api/v3/references/collegiate/json"
+
+  def self.get_word(word)
+    response = get("/#{word}", query: { key: ENV["dictionary_key"] })
+    if response.success?
+        response.parsed_response
+    else
+        { error: "Unable to fetch definition." }
+    end
+  end
+
+  def self.get_definition(word)
+  word_data = self.get_word(word)
+
+  # Check if the response is a valid array of word entries
+  if word_data
+    definition = word_data.first["shortdef"].join("; ")
+    return definition if definition
+  end
+
+  "Definition not available"
+end
+
+end
