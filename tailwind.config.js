1d88d6e64d14ee8eaff9e3b60dd7608c050bea39
Vahnessa Gonzales <vahnessa.gonzales@gmail.com>
2025-06-11 03:43:10 +0800

    setup font family and size modifier feature

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,13 @@ module.exports = {
   ],
   darkMode: 'class', // or 'media'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+        serif: ['"Noto Serif"', 'ui-serif', 'Georgia', 'serif'],
+        mono: ['"Fira Code"', 'ui-monospace', 'monospace']
+      },
+    },
   },
   plugins: [],
 };
