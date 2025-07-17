bbaff73df6b86cf8fbf0b1eb15239654d1e79f94
Vahnessa Gonzales <vahnessa.gonzales@gmail.com>
2025-07-18 02:32:23 +0800

    merge

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,4 @@
 class UsersController < ApplicationController
-<<<<<<< HEAD
-  before_action :authenticate_user!
-  
-    def index; end
-
-    def show; end
-
-    def new
-      @user = User.new
-    end
-
-    def create; end
-
-  private
-
-    def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation)
-    end
-
-=======
     def update_theme
         if current_user.update(theme: params[:theme].to_i)
             render json: { theme: current_user.theme }, status: :ok
@@ -26,5 +6,4 @@ class UsersController < ApplicationController
             render json: { errors: current_user.errors }, status: :unprocessable_entity
         end
     end
->>>>>>> origin/book_branch_2
 end
--- app/wrappers/dictionaryapi.rb
+++ app/wrappers/dictionaryapi.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require 'net/http'
 require 'json'
 require 'uri'
@@ -11,18 +10,4 @@ class Dictionaryapi
   def self.get_words(word)
     get("/#{word}")
   end
-=======
-require 'net/http'
-require 'json'
-require 'uri'
-require 'httparty'
-
-class Dictionaryapi
-  include HTTParty
-  base_uri 'https://api.dictionaryapi.dev/api/v2/entries/en'
-
-  def self.get_words(word)
-    get("/#{word}")
-  end
->>>>>>> origin/book_branch_2
 end
